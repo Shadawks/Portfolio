cf35f2595a22fd5331512c45b5be6ab99fadac04
Shadawks <nathan.leibel@epitech.eu>
2024-12-19 18:17:49 +0100

    Fix display issue with buttons

--- components/user-informations.tsx
+++ components/user-informations.tsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export function UserInformations() {
   return (
-    <div className="flex items-start gap-4 px-2 md:px-10">
+    <div className="flex items-start gap-4 px-1 md:px-10">
 
       <Avatar className="rounded-full border-4 border-[#09090b] -mt-10 w-32 h-32">
         <AvatarImage src="/pdp.jpg" alt="I'll take a picture of myself soon I promise." />
